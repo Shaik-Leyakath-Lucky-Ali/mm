e0f97343345dd13256dbfa35c694fcab76643b24
SHAIK LEYAKATH LUCKY ALI <129724918+Shaik-Leyakath-Lucky-Ali@users.noreply.github.com>
2024-01-12 10:58:13 +0530

    Update ali.py

--- ali.py
+++ ali.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('puppy')
-st.write('puppy')
+st.header('Tony Stark')
+st.write('Iron Man')
 st.image('https://i.pinimg.com/736x/8e/eb/e1/8eebe11cec68e1d4732e71e3f0411cc2.jpg ',caption='ironman')
